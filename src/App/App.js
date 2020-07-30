5dbadad4e6023a02aafb625e31a5559c63cbf5f5
jdvolk <justin.d.volk@gmail.com>
2020-07-30 14:07:26 -0600

    pass cookie props to cookie component

--- src/App/App.js
+++ src/App/App.js
@@ -3,27 +3,41 @@ import './App.css';
 import './MediaQueries.css';
 import { render } from '@testing-library/react';
 import {getOneCookie} from '../ApiCalls';
+import { Cookie } from '../Cookie/Cookie';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      cookies: []
+      cookies: [],
+      isClicked: false
     }
   }
   
+  // componentDidMount = () => {
+  //   this.fetchOneCookie()
+  // }
+
   fetchOneCookie = async  () => {
     const cookie = await getOneCookie();
-    await this.setState({ cookies: [...cookie, ...this.state.cookies]})
+    this.setState({ cookies: [...cookie, ...this.state.cookies]})
+    // this.setState({isClicked: true})
   }
-
+  // handleClick = (props) => {
+    
+  // }
   render() {
     return (
       <div className="App">
         <section className="App-header">
           {/* <NavBar /> */}
           <section className="Main-page">
+          
+            <section className="Cookie-container">
+
+              <Cookie cookies={this.state.cookies} />
+            </section>
             <button 
               className="button"
               onClick={this.fetchOneCookie}
