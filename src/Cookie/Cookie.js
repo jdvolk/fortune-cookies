e7f4b858d6a225b365990b0c3f9f0f57cc8f3c32
jdvolk <justin.d.volk@gmail.com>
2020-07-30 14:06:33 -0600

    display cookie data to page after click

--- src/Cookie/Cookie.js
+++ src/Cookie/Cookie.js
@@ -1,3 +1,32 @@
 import React from 'react';
 import './Cookie.css';
 
+export const Cookie = (props) => {
+  if(props.cookies.length === 0) {
+    return (
+      <p>Press button for cookie</p>
+      )
+    } else {
+      const fortuneCookie = props.cookies[0]
+      console.log(fortuneCookie.fortune.message)
+
+    return ( 
+      <button className="cookie-paper">
+      <section className="Cookie">
+        <section className="Fortune"> 
+          <p>{fortuneCookie.fortune.message}</p>
+        </section> 
+        <section className="Lesson"> 
+          <p> {fortuneCookie.lesson.chinese} </p>
+          <p> {fortuneCookie.lesson.pronunciation} </p>
+          <p> {fortuneCookie.lesson.english} </p>
+        </section> 
+        <section className="Lucky-nums"> 
+         <p>{fortuneCookie.lotto.numbers.toString()}</p>
+       </section>
+      </section>
+      </button>
+
+    )
+  }
+}
