ca1021e311bb6c1ce21d7ddc24f90b0d3f68c1ce
jdvolk <justin.d.volk@gmail.com>
2020-08-03 12:00:22 -0600

    add error handling to display error on coockie paper

--- src/CookiePaper/CookiePaper.js
+++ src/CookiePaper/CookiePaper.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import LeftCookie from '../Assets/cookie-left.png'
-import RightCookie from '../Assets/cookie-right.png'
+// import LeftCookie from '../Assets/cookie-left.png'
+// import RightCookie from '../Assets/cookie-right.png'
 import './CookiePaper.css';
 
 
 export const CookiePaper = (props) => {
+	if(props.error) {
+		var fortuneCookie = {fortune: {message: props.error}, lotto: {numbers: "There was an error retrieving your cookie"}}
+		return (
+			<>
+				<button 
+					className={props.isOpen ? "cookie-paper" : "hidden"}
+					onClick={props.resetError}
+				>
+					<section className="Cookie">
+						<section className="Fortune"> 
+							<p>Sorry! <br /> {fortuneCookie.fortune.message} <br /> Please try again Later</p>
+						</section>
+					</section>
+				</button>
+			</>
+		)
+	}
 	if(props.cookies.length === 0) {
 		return (
 			<p>Press button for cookie</p>
